c9a48b3d767ea035e7077993d807405cae43cd4f
Rodrigo Falcao <rodrigofal44@gmail.com>
2026-02-17 12:40:07 +0000

    WIP.

--- inc/commandHandler/Pass.hpp
+++ inc/commandHandler/Pass.hpp
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Dispatcher.cpp                                     :+:      :+:    :+:   */
+/*   Pass.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/11 21:41:21 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/02/15 18:10:19 by rde-fari         ###   ########.fr       */
+/*   Created: 2026/02/15 17:51:33 by rde-fari          #+#    #+#             */
+/*   Updated: 2026/02/15 17:58:38 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "commands/Dispatcher.hpp"
+#pragma once
 
-std::string dispatch(int fd, const MessagePayload& payload)
-{
-	(void)fd;
-	std::string cmd = payload.command;
-	for (size_t i = 0; i < cmd.length(); i++)
-		cmd[i] = toupper(cmd[i]);
-	if (cmd == "PING")
-	{
-		if (payload.params.size() >= 1)
-			return ("PONG :" + payload.params[0] + "\r\n");
-		return ("PONG\r\n");
-	}
-	return ("");
-}
+#include <string>
+#include <map>
+#include <iostream>
+#include "parser/IrcParser.hpp"
 
-std::string handlePingCommand()
-{
-	
-}
+std::string handlePass(int fd, const MessagePayload& payload);
--- src/CommandHandler/Pass.cpp
+++ src/CommandHandler/Pass.cpp
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Dispatcher.cpp                                     :+:      :+:    :+:   */
+/*   Pass.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/11 21:41:21 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/02/15 18:10:19 by rde-fari         ###   ########.fr       */
+/*   Created: 2026/02/15 17:51:28 by rde-fari          #+#    #+#             */
+/*   Updated: 2026/02/15 18:08:37 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "commands/Dispatcher.hpp"
+#include "commandHandler/Pass.hpp"
 
-std::string dispatch(int fd, const MessagePayload& payload)
-{
-	(void)fd;
-	std::string cmd = payload.command;
-	for (size_t i = 0; i < cmd.length(); i++)
-		cmd[i] = toupper(cmd[i]);
-	if (cmd == "PING")
-	{
-		if (payload.params.size() >= 1)
-			return ("PONG :" + payload.params[0] + "\r\n");
-		return ("PONG\r\n");
-	}
-	return ("");
-}
-
-std::string handlePingCommand()
+std::string handlePass(int fd, const MessagePayload& payload)
 {
 	
+	return ("");
 }
