b045b46732401dbc31ca209a8e7dde83e1518aa7
Rodrigo Falcao <rodrigofal44@gmail.com>
2026-02-17 12:40:07 +0000

    WIP.

--- inc/commandHandler/Pass.hpp
+++ inc/commandHandler/Pass.hpp
@@ -1,39 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   UserRepository.hpp                                 :+:      :+:    :+:   */
+/*   Pass.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/12 23:10:59 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/02/15 17:44:46 by rde-fari         ###   ########.fr       */
+/*   Created: 2026/02/15 17:51:33 by rde-fari          #+#    #+#             */
+/*   Updated: 2026/02/15 17:58:38 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-
 #include <string>
 #include <map>
+#include <iostream>
 #include "parser/IrcParser.hpp"
 
-struct User
-{
-	int fileDescriptor;
-	std::string username;
-	std::string password;
-};
-
-class UserRepository
-{
-	private:
-		std::map<std::string, User> usersByUsername;
-		std::map<int, User> usersByFileDescriptor;
-
-	public:
-		User findUserByUsername(std::string username);
-		User findUserByFileDescriptor(int fd);
-		bool createUser(int fd, std::string username, std::string password);
-		bool removeUserByUsername(std::string username);
-		bool removeUserByFileDescriptor(int fd);
-};
+std::string handlePass(int fd, const MessagePayload& payload);
